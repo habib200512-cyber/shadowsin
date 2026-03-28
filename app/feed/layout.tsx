14fb857c6c3565c3959182cb479cc7fcceaa26f2
Dusdef <dusdef@outlook.com>
2026-03-27 23:25:17 -0600

    fix: typescript types in layouts

--- app/feed/layout.tsx
+++ app/feed/layout.tsx
@@ -8,7 +8,7 @@ export default async function FeedLayout({ children }: { children: React.ReactNo
   if (!session) redirect("/login");
   return (
     <div className="min-h-dvh bg-[#F0EDE8] flex flex-col">
-      <Navbar user={session.user} />
+      <Navbar user={session.user ?? {}} />
       <main className="flex-1 pb-20 pt-2">{children}</main>
       <BottomNav />
     </div>
